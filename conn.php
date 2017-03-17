de980c46b7412857c49a2b4b9dee6a417d73b4c2
brijeshpatil <brijesh.patil@tops-int.com>
2017-03-17 15:36:46 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn=new MySQLi("localhost","root","","u164511771_art");
+$conn=new MySQLi("bdc60d26-ae13-4ce4-9f7e-a73900a5af5c.mysql.sequelizer.com","eqlzgionqzpbpwhp","6VEWrhcGrQ5bBeK8mmamHHdkwNfkATz5ba3R64rdJ3LGyFdJtLYxZedvRuyKbYgF","dbbdc60d26ae134ce49f7ea73900a5af5c");
 
 ?>
